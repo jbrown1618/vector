301fa6790b5cbc4d9cea109dfa82a2ce159dc625
Josh Brown <josh.brown@sas.com>
2018-06-27 13:30:05 -0400

    Resolved bugs in Row Echelon Form algorithm

--- src/algorithms/RowEchelon.spec.ts
+++ src/algorithms/RowEchelon.spec.ts
@@ -6,10 +6,30 @@ import { rowEchelonForm } from "./RowEchelon";
 
 describe("RowEchelon", () => {
   describe("rowEchelonForm", () => {
-    it("row reduces a matrix", () => {
+    it("row reduces a 'wide' matrix", () => {
       const A = Matrix.fromData([[1, 2, 3], [4, 5, 6]]);
-      const ref = rowEchelonForm(A);
-      expect(ref.getData()).to.deep.equal([[1, 2, 3], [0, 1, 2]]);
+      const Aref = rowEchelonForm(A);
+      expect(Aref.getData()).to.deep.equal([[1, 2, 3], [0, 1, 2]]);
+    });
+
+    it("row reduces a 'tall' matrix", () => {
+      const A = Matrix.fromData([[0, 1], [0, 0], [5, 9]]);
+      const Aref = rowEchelonForm(A);
+      expect(Aref.getData()).to.deep.equal([[1, 9 + 9 * (0.2 - 1)], [0, 1], [0, 0]]);
+    });
+
+    it("row reduces a matrix with non-independent rows", () => {
+      const A = Matrix.fromData([[1, 2, 3], [1, 1, 1], [1, 1, 1]]);
+
+      const Aref = rowEchelonForm(A);
+      expect(Aref.getData()).to.deep.equal([[1, 2, 3], [0, 1, 2], [0, 0, 0]]);
+    });
+
+    it("row reduces a matrix with non-diagonal pivot entries", () => {
+      const A = Matrix.fromData([[1, 1, 1], [0, 0, 2], [0, 0, 1]]);
+
+      const Aref = rowEchelonForm(A);
+      expect(Aref.getData()).to.deep.equal([[1, 1, 1], [0, 0, 1], [0, 0, 0]]);
     });
 
     it("does nothing to an identity matrix", () => {
--- src/algorithms/RowEchelon.ts
+++ src/algorithms/RowEchelon.ts
@@ -16,18 +16,24 @@ export function reducedRowEchelonForm(matrix: Matrix): Matrix {
 export function rowEchelonForm(matrix: Matrix): Matrix {
   matrix = moveLeadingZerosToBottom(matrix);
 
-  const numberOfPivotEntries = Math.min(matrix.getNumberOfColumns(), matrix.getNumberOfRows());
-  for (let pivotIndex = 0; pivotIndex < numberOfPivotEntries; pivotIndex++) {
-    const pivotEntry = matrix.getEntry(pivotIndex, pivotIndex);
+  const maxNumberOfPivotEntries = Math.min(matrix.getNumberOfRows(), matrix.getNumberOfColumns());
+  for (let pivotRow = 0; pivotRow < maxNumberOfPivotEntries; pivotRow++) {
+    let pivotColumn = pivotRow;
+    let pivotEntry = matrix.getEntry(pivotRow, pivotColumn);
+
+    while (pivotEntry == 0 && pivotColumn < matrix.getNumberOfColumns() - 1) {
+      pivotEntry = matrix.getEntry(pivotRow, ++pivotColumn);
+    }
+
     if (pivotEntry === 0) {
       continue;
     }
 
     if (pivotEntry !== 1) {
-      matrix = RowOperations.multiplyRowByScalar(matrix, pivotIndex, 1 / pivotEntry);
+      matrix = RowOperations.multiplyRowByScalar(matrix, pivotRow, 1 / pivotEntry);
     }
 
-    matrix = clearEntriesBelow(matrix, pivotIndex);
+    matrix = clearEntriesBelow(matrix, pivotRow, pivotColumn);
   }
 
   return matrix;
@@ -53,30 +59,34 @@ function moveLeadingZerosToBottom(matrix: Matrix): Matrix {
   return Matrix.fromData(matrix.getData().sort(comparator));
 }
 
-function clearEntriesBelow(matrix: Matrix, pivotIndex: number): Matrix {
-  checkPreconditionsForClearingBelow(matrix, pivotIndex);
+function clearEntriesBelow(matrix: Matrix, pivotRow: number, pivotColumn: number): Matrix {
+  checkPreconditionsForClearingBelow(matrix, pivotRow, pivotColumn);
 
-  for (let rowIndex = pivotIndex + 1; rowIndex < matrix.getNumberOfRows(); rowIndex++) {
-    const entry = matrix.getEntry(rowIndex, pivotIndex);
+  for (let rowIndex = pivotRow + 1; rowIndex < matrix.getNumberOfRows(); rowIndex++) {
+    const entry = matrix.getEntry(rowIndex, pivotColumn);
     if (entry === 0) {
       continue;
     }
 
-    matrix = RowOperations.addScalarMultipleOfRowToRow(matrix, rowIndex, pivotIndex, -1 * entry);
+    matrix = RowOperations.addScalarMultipleOfRowToRow(matrix, rowIndex, pivotRow, -1 * entry);
   }
 
   return matrix;
 }
 
-function checkPreconditionsForClearingBelow(matrix: Matrix, pivotIndex: number): void {
+function checkPreconditionsForClearingBelow(
+  matrix: Matrix,
+  pivotRow: number,
+  pivotColumn: number
+): void {
   // The pivot entry should be 1
-  if (matrix.getEntry(pivotIndex, pivotIndex) !== 1) {
+  if (matrix.getEntry(pivotRow, pivotColumn) !== 1) {
     throw Error("Not ready yet!");
   }
 
   // Values to the left of the pivot should be 0
-  for (let i = 0; i < pivotIndex - 1; i++) {
-    if (matrix.getEntry(pivotIndex, i) !== 0) {
+  for (let i = 0; i < pivotColumn - 1; i++) {
+    if (matrix.getEntry(pivotRow, i) !== 0) {
       throw Error("Not ready yet!");
     }
   }
