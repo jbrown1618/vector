ac359d1d989468a1b47565ec8d6eadfcd9b1e710
Josh Brown <josh.brown@sas.com>
2018-07-13 13:48:01 -0400

    Implemented matrix determinants using expansion of minors

---
+++ src/algorithms/Determinant.ts
@@ -0,0 +1,22 @@
+import { Matrix, MatrixBuilder } from '..';
+import { assertSquare } from '../utilities/ErrorAssertions';
+
+export function determinant(matrix: Matrix<number>): number {
+  assertSquare(matrix);
+
+  // Definition of a 2x2 determinant
+  if (matrix.getNumberOfRows() === 2) {
+    return (
+      matrix.multiplyScalars(matrix.getEntry(0, 0), matrix.getEntry(1, 1)) -
+      matrix.multiplyScalars(matrix.getEntry(0, 1), matrix.getEntry(1, 0))
+    );
+  }
+
+  // Calculate NxN determinant by expansion of minors
+  let det = 0;
+  for (let j = 0; j < matrix.getNumberOfColumns(); j++) {
+    const sign = Math.pow(-1, j);
+    det += matrix.getEntry(0, j) * determinant(MatrixBuilder.exclude(matrix, 0, j)) * sign;
+  }
+  return det;
+}
