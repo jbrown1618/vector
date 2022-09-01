04337520fc037a18dabdfdb059e0549f075d6e96
jbrown1618 <jbrown1618@github.com>
2022-09-01 16:46:58 -0400

    Update versions of dependencies

--- test/utils/testData.ts
+++ test/utils/testData.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import csvParser from 'csv-parse/lib/sync';
-import csvWriter from 'csv-stringify/lib/sync';
+import { parse } from 'csv-parse/sync';
+import { stringify } from 'csv-stringify/sync';
 import { Matrix } from '../../src/types/matrix/Matrix';
 import { mat } from '../../src/utilities/aliases';
 
@@ -11,11 +11,11 @@ export function loadTestData(name: string): Matrix {
   if (cache.has(name)) return cache.get(name) as Matrix;
 
   const contents = fs.readFileSync(path.resolve(__dirname, '../data/', name + '.csv')).toString();
-  const data = (csvParser(contents) as any[][]).map((row) => row.map((entry) => parseFloat(entry)));
+  const data = (parse(contents) as any[][]).map((row) => row.map((entry) => parseFloat(entry)));
   return mat(data);
 }
 
 export function writeTestData(name: string, matrix: Matrix): void {
-  const contents = csvWriter(matrix.toArray());
+  const contents = stringify(matrix.toArray());
   fs.writeFileSync(path.resolve(__dirname, '../data/', name + '.csv'), contents);
 }
